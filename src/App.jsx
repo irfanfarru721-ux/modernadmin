24490989c15206bb2c49eefe928dcef729c7c3ae
irfanfarru721-ux <irfanfarru721@gmail.com>
2025-12-09 14:54:51 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,15 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import AdminLogin from './pages/AdminLogin';
-import Dashboard from './pages/Dashboard';
-import Categories from './pages/Categories';
-import Modules from './pages/Modules';
-import Vendors from './pages/Vendors';
-import Products from './pages/Products';
-import Users from './pages/Users';
-import Orders from './pages/Orders';
-import ProtectedRoute from './components/ProtectedRoute';
-import AdminLayout from './components/AdminLayout';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard.jsx";
+import Login from "./pages/Login.jsx";
+import Vendors from "./pages/Vendors.jsx";
 
-export default function App(){
- return (
-  <BrowserRouter>
-   <Routes>
-     <Route path='/admin/login' element={<AdminLogin/>}/>
-     <Route path='/admin' element={<ProtectedRoute><AdminLayout/></ProtectedRoute>}>
-        <Route path='dashboard' element={<Dashboard/>}/>
-        <Route path='modules' element={<Modules/>}/>
-        <Route path='vendors' element={<Vendors/>}/>
-        <Route path='categories' element={<Categories/>}/>
-        <Route path='products' element={<Products/>}/>
-        <Route path='users' element={<Users/>}/>
-        <Route path='orders' element={<Orders/>}/>
-     </Route>
-   </Routes>
-  </BrowserRouter>
- );
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/vendors" element={<Vendors />} />
+    </Routes>
+  );
 }
