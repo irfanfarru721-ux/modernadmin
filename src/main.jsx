4a5ae82578c6673ba498be9626c9ffa2a52fd13b
irfanfarru721-ux <irfanfarru721@gmail.com>
2025-12-09 15:03:04 +0530

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,1 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { AuthProvider } from "./context/AuthContext";
-import { CartProvider } from "./context/CartContext";
-import "./index.css";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <AuthProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </AuthProvider>
-  </React.StrictMode>
-);
+import React from 'react'; import ReactDOM from 'react-dom/client'; import App from './App.jsx'; ReactDOM.createRoot(document.getElementById('root')).render(<App/>);
