a2a6ff80c62b34ca319ba92f34356653c4bdcdf9
irfanfarru721-ux <irfanfarru721@gmail.com>
2025-12-09 14:39:33 +0530

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,1 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import AdminLogin from "./pages/AdminLogin";
-import Dashboard from "./pages/Dashboard";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/admin" element={<AdminLogin />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-    </Routes>
-  </BrowserRouter>
-);
+import React from 'react'; import ReactDOM from 'react-dom/client'; import App from './App.jsx'; ReactDOM.createRoot(document.getElementById('root')).render(<App/>);
