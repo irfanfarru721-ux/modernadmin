fe34cd8dc0f70407de9cffa12af6327421af197e
irfanfarru721-ux <irfanfarru721@gmail.com>
2025-12-11 17:12:56 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
-const API = axios.create({ baseURL: import.meta.env.VITE_BACKEND_URL ? import.meta.env.VITE_BACKEND_URL + '/api' : 'http://localhost:5000/api' });
+const API = axios.create({ baseURL: import.meta.env.VITE_BACKEND_URL ? import.meta.env.VITE_BACKEND_URL + '/api' : 'https://backendmodern.onrender.com/api' });
 API.interceptors.request.use((config)=>{ const token = typeof window !== 'undefined' ? localStorage.getItem('adminToken') : null; if(token) config.headers.Authorization = `Bearer ${token}`; return config; });
 export default API;
